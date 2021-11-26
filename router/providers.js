811068956c767cfae10d51761ec92cd7ecb0e55b
unknown <zim21den21@gmail.com>
2021-11-26 22:57:35 +0200

    added update provider

--- router/providers.js
+++ router/providers.js
@@ -10,7 +10,7 @@ router.get('/all', async (req, res) => {
         return res.json(providers)
     } catch (error) {
         console.log(error);
-        return res.status(500).json({error})
+        return res.status(500).json({error});
     }
 });
 
@@ -42,5 +42,22 @@ router.post('/create',
     }
     
 });
+
+router.put('/update',
+    body('email').isEmail().withMessage("invalid email"),
+    body('password').isLength({ min: 5 }).withMessage("password to short"),
+    async (req, res) => {
+        const find = url.parse(req.url, true).query;
+        const { username } = req.body;
+        const { password } = req.body;
+        const { email } = req.body;
+        try {
+            const updateProvider = await Provider.findOneAndUpdate(find.username, 
+                { $set: { username, password, email } }, { new: true });
+            return res.json(updateProvider);
+        } catch (error) {
+            res.status(400).json({ error })
+    }
+})
 
 module.exports = router;
